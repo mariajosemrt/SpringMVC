5faf42910ffaa2fbe708374d2bef61686d56e5dc
mariajosemrt <andreumj7@gmail.com>
2023-03-21 14:30:26 +0100

    Soncronizado con la rama formulario

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -63,5 +64,13 @@ public class MainController {
 
         return "views/formularioAltaEstudiante"; //tenemos que poner la vista (crearla) donde está
     } 
+
+    /**
+     * Metodo que recibe los datos procedentes de los controles del formulario
+     */
+    @PostMapping("/altaEstudiante")
+    public void altaEstudiante() {
+
+    }
 
 }
